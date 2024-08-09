840eed42e24e7278c2c8aaff7038112e0ed2b202
Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
2024-08-09 16:28:53 +0800

    feat(session-01): Update notes and add examples

---
+++ session-01/empty.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * FILE TITLE GOES HERE
+ *
+ * DESCRIPTION OF THE PURPOSE AND USE OF THE CODE
+ * MAY BE MORE THAN ONE LINE LONG
+ * KEEP LINE LENGTH TO NO MORE THAN 96 CHARACTERS
+ *
+ * Filename:        empty.php
+ * Location:        ${FILE_LOCATION}
+ * Project:         SaaS-FED-Notes
+ * Date Created:    9/08/2024
+ *
+ * Author:          YOUR_NAME <YOUR_EMAIL_ADDRESS>
+ *
+ */
